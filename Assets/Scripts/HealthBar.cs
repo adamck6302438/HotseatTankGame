3779909b1ceb4792ed54cf86a68fa8a60731584e
Sal <chomuham@sheridancollege.ca>
2021-04-14 23:56:00 -0400

    Health Display
    -Created HealthBar.cs
    -Created two cube objects and placed them above each tank
    -Attached HealthBar.cs to each of them
    -Cube objects are above tanks
    -Created two separate materials and attached them to cube objects for coloring
    -Coded HealthBar.cs to get health from a tank and update it in real-time

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    //Fields
+    public string tankColour; //colour of tank below healthBar
+    public float healthSize; //size of healthBar 
+    public GameObject tank; //GameObject of tank below the health bar
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tank = GameObject.Find($"TankFree_{tankColour}");
+        healthSize = tank.GetComponent<Tank>().health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale = new Vector3(healthSize, 1, 1);
+    }
+}
