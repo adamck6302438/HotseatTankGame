d0ebaf2e957590396e4b923426ecf6ac61f51781
Sal <chomuham@sheridancollege.ca>
2021-04-17 22:35:11 -0400

    minor changes
    -update healthbar.cs
    -update position of ui components

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -12,13 +12,18 @@ public class HealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tank = GameObject.Find($"TankFree_{tankColour}");
-        healthSize = tank.GetComponent<Tank>().health;
+        tank = GameObject.Find($"TankFree_{tankColour}"); //select tank object based on color
+        healthSize = tank.GetComponent<Tank>().health; //size of healthbar is selected from the tank object chosen
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.localScale = new Vector3(tank.GetComponent<Tank>().health, 1, 1);
+        this.transform.position = new Vector3(
+            tank.transform.position.x,
+            this.transform.position.y,
+            this.transform.position.z
+            ); //healthbar moves with tank
+        this.transform.localScale = new Vector3(healthSize, 1, 1); //healthbar size adjusts to health of tank
     }
 }
